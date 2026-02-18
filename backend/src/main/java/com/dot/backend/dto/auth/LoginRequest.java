e341349315680620bb9f74e853ca39aa2864b3e6
hyunil <jackandjills@naver.com>
2026-02-18 14:28:45 +0900

    [Feat] 백엔드 깃허브 엑션 테스트를 위한 메시지 수정

--- backend/src/main/java/com/dot/backend/dto/auth/LoginRequest.java
+++ backend/src/main/java/com/dot/backend/dto/auth/LoginRequest.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class LoginRequest {
 
-    @NotBlank(message = "이메일을 입력해주세요")
+    @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "올바른 이메일 형식이 아닙니다")
     private String email;
 
